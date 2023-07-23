5af868bf9a290fd185b617173c2bd9034331566f
mrlegend99 <mrahman.ruet16@gmail.com>
2023-07-23 22:56:51 +0600

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class="container mx-auto">
+      <h1 class="font-bold text-2xl mt-10 text-center">Welcome.</h1>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
